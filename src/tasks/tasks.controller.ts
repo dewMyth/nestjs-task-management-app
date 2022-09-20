210bfeae93c06791fb8e29139bdadf44bb631313
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-09-20 18:38:33 +0530

    basic REST CRUD (No DB)

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import { Task, TaskStatus } from './task.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TaskController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get(':taskId')
+  getSingleTask(@Param('taskId') id: string): Task {
+    return this.tasksService.getSingleTask(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDTO: CreateTaskDTO): Task {
+    return this.tasksService.createTask(createTaskDTO);
+  }
+
+  @Delete(':taskId')
+  deleteTask(@Param('taskId') id: string): Task[] {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch(':taskId/status')
+  updateTaskStatus(
+    @Param('taskId') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+}
