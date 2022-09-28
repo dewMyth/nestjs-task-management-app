05c9cab48bda84a4152fdbfdc1c3b522008b4e08
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-09-28 09:25:40 +0530

    Added signup with validations/pw hashing

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  // User Signup
+  async signUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
+    const { username, password } = authCredentialsDTO;
+
+    const user = new User();
+
+    user.username = username;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+
+    console.log(user.password);
+
+    try {
+      await user.save();
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  //Password Hashing
+  private async hashPassword(password: string, salt: string) {
+    return bcrypt.hash(password, salt);
+  }
+}
